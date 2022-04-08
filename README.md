1ae5f9a18179f7dc0fe44e48095c9074f5e41a90
Terone <bterone@gmail.com>
2022-04-08 10:13:04 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center">
+  <img alt="Nimble logo" src="https://assets.nimblehq.co/logo/light/logo-light-text-320.png" />
+</p>
+
+<p align="center">
+  <strong>Our Github Action publishes your documentation to your project's Github Wiki!</strong>
+</p>
+
+---
+
+<!-- TODO: Add Action Usage Instructions -->
+<!-- TODO: Add Setup Instructions, e.g. Generating a Personal Access Token -->
---
+++ entrypoint.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+REPOSITORY=$GITHUB_REPOSITORY
+WIKI_FOLDER=".github/wiki"
+TEMP_CLONE_WIKI_FOLDER="tmp_wiki"
+COMMIT_MSG="Update Wiki content"
+
+function error() {
+  echo "::error file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$1"
+}
+
+function add_mask() {
+  echo "::add-mask::$1"
+}
+
+if [ -z "$USER_NAME" ]; then
+    error "USER_NAME environment variable is not set"
+    exit 1
+fi
+
+if [ -z "$USER_EMAIL" ]; then
+    error "USER_EMAIL environment variable is not set"
+    exit 1
+fi
+
+if [ -z "$USER_TOKEN" ]; then
+    error "USER_TOKEN environment variable is not set"
+    exit 1
+fi
+
+add_mask "${USER_TOKEN}"
+
+# This step will
+# - Create folder named `tmp_wiki`
+# - Initialize Git
+# - Pull old Wiki content
+{
+  mkdir $TEMP_CLONE_WIKI_FOLDER
+  cd $TEMP_CLONE_WIKI_FOLDER || exit 1
+  git init
+  git config user.name $USER_NAME
+  git config user.email $USER_EMAIL
+  git pull https://$USER_TOKEN@github.com/$REPOSITORY.wiki.git
+  cd ..
+} || exit 1
+
+# This step will
+# - Synchronize differences between `.github/wiki/` & `tmp_wiki`
+# - Push new Wiki content
+{
+  rsync -av --delete $WIKI_FOLDER/ $TEMP_CLONE_WIKI_FOLDER/ --exclude .git
+  cd $TEMP_CLONE_WIKI_FOLDER
+  git add .
+  git commit -m $COMMIT_MSG
+  git push -f --set-upstream https://$USER_TOKEN@github.com/$REPOSITORY.wiki.git master
+} || exit 1
+
+rm -rf "$TEMP_CLONE_WIKI_FOLDER"
+exit 0
